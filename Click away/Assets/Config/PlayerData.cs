239630c8302db3d0bbfaa7e5f8c92a2a148240b5
Dmytro Mazniak <dmytromazniak0@gmail.com>
2026-08-29 17:44:45 +0200

    idk

--- Click away/Assets/Config/PlayerData.cs
+++ Click away/Assets/Config/PlayerData.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/PlayerData")]
 public class PlayerData : ScriptableObject
 {
-     [field:SerializeField]  public float money { get; set; } 
+     [field:SerializeField]  public float money { get; set; }
+    [field:SerializeField] public float bulletSpeed { get;set; }
+    [field:SerializeField] public float bulletDamage { get; set; }
+    [field:SerializeField]  public float ArrowSpeed { get; set; }
+    [field: SerializeField] public float arrowCurrentDamage { get; set; }
 
-    
-    
+
+
 }
---
+++ Click away/Assets/Scripts/PlayerSaveData - kopie.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerSaveData
+{
+    public int level;
+    public int score;
+    public float posX;
+    public float posY;
+    public float posZ;
+}
